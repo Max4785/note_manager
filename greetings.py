28adfa8b40789e7e0aa3f36555deedce9585decd
Max4785 <m.brikmann@bk.ru>
2025-01-04 20:13:52 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Max"
+title = "Проверка значений"
+content = "Напечатать с помощью функции print"
+status = "В работе"
+created_date = "4-1-2025"
+issue_date = "5-1-2025"
+print(f"Имя пользователя: {username}",
+      f"Заголовок заметки: {title}",
+      f"Описание заметки: {content}",
+      f"Статус заметки: {status}",
+      f"Дата создания заметки: {created_date}",
+      f"Дата истечения заметки: {issue_date}",
+      sep= '\n')
